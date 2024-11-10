9b1a2527f4fb208f9cc37ad3dbc2c24e58007648
Adham20022024 <adham54732@gmail.com>
2024-11-10 08:04:50 +0200

    use namespaces make groups

--- public/js/chat.js
+++ public/js/chat.js
@@ -1,35 +1,80 @@
+// Broadcast a message to connected users when someone connects or disconnects
+// Add “{user} is typing” functionality
+// Show who’s online
+// Add private messaging
+// Share your improvements!
+
 window.addEventListener("load", () => {
-  console.log("loaded");
-  const socket = io("http://localhost:8000");
-  window.addEventListener("unload", () => {
-    socket.disconnect();
-    //redirect to another page
-  });
+  const $loginForm = document.getElementById("loginForm");
+  const $nameInput = document.getElementById("nameInput");
   const $messageInput = document.getElementById("messageInput");
-  const $form = document.getElementById("messageForm");
+  const $messageForm = document.getElementById("messageForm");
   const $messagesContainer = document.getElementById("messagesContainer");
-  // send message
-  $form.addEventListener("submit", (e) => {
-    e.preventDefault();
+  const $title = document.getElementById("title");
+
+  // get namespace from the url
+  let url = location.pathname.split("/");
+  let namespace = url[url.length - 1];
+  $title.innerText = `Namespace: ${namespace}`;
+  document.title = namespace;
+
+  // socket is the global object used to listen on incoming messages
+  // and send (emit) ones to the server.
+  let socket;
+  // username is used to be compared with 'from' in 'msg' events
+  let username;
+  function login(name) {
+    // Create a socket connection
+    socket = ioConnect();
+    username = name;
+    socket.emit("login", name);
+  }
+
+  // Login
+  $loginForm.addEventListener("submit", function (event) {
+    event.preventDefault();
+    // Login with `name`
+    let name = $nameInput.value;
+    login(name);
+    // Remove the login form and
+    // show the chat message form
+    $loginForm.remove();
+    $messageForm.classList.remove("hidden");
+  });
+
+  // Send Message
+  $messageForm.addEventListener("submit", function (event) {
+    event.preventDefault();
     let message = $messageInput.value;
     $messageInput.value = "";
-    //send
+    // Send
     socket.emit("msg", message);
   });
-  // receive message
-  socket.on("msg", (data) => {
-    console.log("happened");
-    console.log(data);
-    if (data.from != socket.id) {
-      say(data.from, data.message);
-    } else {
-      say("me", data.message);
-    }
-  });
+
+  function ioConnect() {
+    // Connect to a namespace
+    let socket = io("/" + namespace);
+    // Handle exiting page
+    window.onunload = () => {
+      socket.close();
+    };
+
+    // Recieve Message
+    socket.on("msg", (data) => {
+      if (data.from != username) {
+        say(data.from, data.message);
+      } else {
+        say("me", data.message);
+      }
+    });
+
+    return socket;
+  }
+
   function say(name, message) {
     $messagesContainer.innerHTML += `<div class="chat-message">
-        <span style="color: red; font-weight: bold;">${name}:</span> ${message}
-    </div>`;
+          <span style="color: red; font-weight: bold;">${name}:</span> ${message}
+      </div>`;
     // Scroll down to last message
     $messagesContainer.scrollTop = $messagesContainer.scrollHeight;
   }
--- server.js
+++ server.js
@@ -1,51 +1,91 @@
-import express from "express";
-import { Server } from "socket.io";
-import chalk from "chalk";
-import path from "path";
-
+const express = require("express");
 const app = express();
-const __dirname = path.resolve();
+const http = require("http").Server(app);
+const io = require("socket.io")(http);
+const path = require("path");
+const logger = require("morgan");
+const bodyParser = require("body-parser");
 
-// View engine setup
+const port = process.env.PORT || 8000;
+app.set("port", port);
+
+http.listen(port, () => {
+  console.log("listening on port " + port);
+});
+
+// view engine setup
+//test routes
+console.log(__dirname);
 app.set("view engine", "ejs");
 app.set("views", path.join(__dirname, "views"));
-
-// Serve static files from the "public" directory
 app.use(express.static(path.join(__dirname, "public")));
 
-const PORT = process.env.PORT || 8000;
-const server = app.listen(PORT, () => {
-  console.log(
-    `Server is running on` + chalk.red.bold(` http://localhost:${PORT}`)
-  );
-});
+// middleware
+app.use(logger("dev"));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
 
-// Route to render the chat page
-app.get("/chat", (req, res) => {
-  res.render("chat"); // Ensure there's a "chat.ejs" in the "views" folder
-});
+let namespaces = ["one", "two", "three"];
 
-// Enable server to support socket connection
-const io = new Server(server);
+for (let i = 0; i < namespaces.length; i++) {
+  const ns = io.of("/" + namespaces[i]);
+  // users is a key-value pairs of socket.id -> user name
+  let users = {};
+  ns.on("connection", (socket) => {
+    // Every socket connection has a unique ID
+    console.log("new connection: " + socket.id);
 
-io.on("connection", (socket) => {
-  // check if Client go online
-  console.warn("New client connected", socket.id);
-  socket.on("msg", (message) => {
-    console.log(message);
-    // send message to all clients (except the sender)
-    socket.broadcast.emit("msg", {
-      from: socket.id,
-      message,
+    // User Logged in
+    socket.on("login", (name) => {
+      // Map socket.id to the name
+      users[socket.id] = name;
+
+      // Broadcast to everyone else (except the sender).
+      // Say that the user has logged in.
+      socket.broadcast.emit("msg", {
+        from: "server",
+        message: `${name} logged in.`,
+      });
     });
-    // send message to only the sender
-    socket.emit("msg", {
-      from: socket.id,
-      message,
+
+    // Message Recieved
+    socket.on("msg", (message) => {
+      // Broadcast to everyone else (except the sender)
+      socket.broadcast.emit("msg", {
+        from: users[socket.id],
+        message: message,
+      });
+      // Send back the same message to the sender
+      socket.emit("msg", {
+        from: users[socket.id],
+        message: message,
+      });
+      // You could just do: io.emit('msg', ...)
+      // which will send the message to all, including
+      // the sender.
+    });
+
+    // Disconnected
+    socket.on("disconnect", function () {
+      // Remove the socket.id -> name mapping of this user
+      console.log("disconnect: " + users[socket.id]);
+      delete users[socket.id];
+      // io.emit('disconnect', socket.id)
     });
   });
-  // check if Client go offline
-  socket.on("disconnect", () => {
-    console.warn("Client disconnected", socket.id);
+}
+
+// Routes
+app.get("/lobby", (req, res) => {
+  res.render("lobby", {
+    namespaces: namespaces,
   });
+});
+
+app.get("/chat/:namespace", (req, res) => {
+  let namespace = req.params.namespace;
+  if (!namespaces.includes(namespace)) {
+    return res.sendStatus(404);
+  }
+  res.render("chat");
 });
